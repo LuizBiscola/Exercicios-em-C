e920b3b545b509778fc4435ab83aec333ac35a76
Luiz Eduardo <163674230+LuizBiscola@users.noreply.github.com>
2024-09-15 15:19:04 -0300

    Create nota_escolar.c

---
+++ nota_escolar.c
@@ -0,0 +1,19 @@
+// saber se o estudante está aprovado ou reprovado 
+#include <stdio.h>
+#include <locale.h>
+int main (void)
+
+{
+    setlocale(LC_ALL,"Portuguese");
+    float nota;
+    printf ("Informe a nota: ");
+    scanf("%f", &nota);
+    if (nota >= 6)
+    {
+            printf("Aprovado");
+     }
+     else
+     {
+         printf("Repovado");
+     }
+}
